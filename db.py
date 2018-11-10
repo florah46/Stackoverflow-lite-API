a3fe3c4dd2dfc985bdd59456fa24319361d90690
florah46 <42272220+florah46@users.noreply.github.com>
2018-11-10 05:04:16 -0800

    Update db.py

--- db.py
+++ db.py
@@ -13,7 +13,7 @@ class DatabaseConnection:
 
         try:
             self.connection = psycopg2.connect(
-                dbname='d62ol3dfvvnubk', user='rkgsgupxsprnfx', host='ec2-174-129-236-147.compute-1.amazonaws.com', password='77c80267f50cfc06bf5f8d89b27bf08df304c9c11c6c70c6337193fa77605a12', port='5432'
+                dbname='d1qrq65a9ek8oa', user='lqiposwrdwiagc', host='ec2-23-23-101-25.compute-1.amazonaws.com', password='885e297b00845855066f67dd1e5d97232696e18ce0cc377ccec97ba31eb47ed8', port='5432'
             )
 
             self.connection.autocommit = True
